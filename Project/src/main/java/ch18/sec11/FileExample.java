f3d2144bf9e479adae818804cb6005dcada5d079
glorypnag <dys1128@naver.com>
2025-05-10 15:37:09 +0900

    20250510 심화

--- Project/src/main/java/ch18/sec11/FileExample.java
+++ Project/src/main/java/ch18/sec11/FileExample.java
@@ -1,32 +1,20 @@
 package ch18.sec11;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class FileExample {
     public static void main(String[] args) throws Exception {
-        File dir = new File("C:/Temp/images");
-        File file1 = new File("C:/Temp/file1.txt");
-        File file2 = new File("C:/Temp/file2.txt");
-        File file3 = new File("C:/Temp/file3.txt");
+        File file = new File("C:/Temp/file1.txt");
 
-        if(dir.exists() == false) { dir.mkdirs(); }
-        if(file1.exists() == false) { file1.createNewFile(); }
-        if(file2.exists() == false) { file2.createNewFile(); }
-        if(file3.exists() == false) { file3.createNewFile(); }
-
-        File temp = new File("C:/Temp");
-        File[] contents = temp.listFiles();
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a HH:mm");
-        for(File file : contents) {
-            System.out.printf("%-25s", sdf.format(new Date(file.lastModified())));
-            if(file.isDirectory()) {
-                System.out.printf("%-10s%-20s", "<DIR>", file.getName());
-            } else {
-                System.out.printf("%-10s%-20s", file.length(), file.getName());
+        if (file.exists()) {
+            if (file.isFile()) {
+                System.out.println("파일 경로: " + file.getAbsolutePath());
+                System.out.println("파일 크기: " + file.length() + " bytes");
+            } else if (file.isDirectory()) {
+                System.out.println("<dir> " + file.getAbsolutePath());
             }
-            System.out.println();
+        } else {
+            System.out.println("해당 파일은 없는 파일입니다.");
         }
     }
 }
